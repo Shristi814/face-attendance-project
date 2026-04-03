9709f044ea7092fa0805d2b87f647c8e4e37895c
Pragyaa-pg <aaryapg17@gmail.com>
2026-04-03 13:11:51 +0530

    fixed mongo_uri error

--- server.js
+++ server.js
@@ -26,10 +26,7 @@ app.use(bodyParser.json());
 // ------------------------
 // MongoDB Connection
 // ------------------------
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error("DB Connection Error:", err));
 
